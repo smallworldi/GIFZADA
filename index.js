81294efd037933de5a4e3e980cc19ab1a9ccd5a2
viktoria kuznetsova <viktoriakuznetsov@mail.ru>
2025-04-26 20:49:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on('messageCreate', async message => {
 ・As conversões são automáticas, práticas e feitas dentro da thread privada;  
 ・Tudo acontece de forma simples, rápida e sem complicação aqui no **GIFZADA**!
 `)
-
+      .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
       .setColor('DarkButNotBlack');
 
     const row = new ActionRowBuilder().addComponents(
